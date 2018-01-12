8cc341678404800e13183057f6a0aa5d7d5796bb
wuyinlei <power1118wu@gmail.com>
2018-01-12 11:06:05 +0800

    spring boot搭建web项目简单测试

---
+++ src/main/java/com/ruolan/springtest/SpringtestApplication.java
@@ -0,0 +1,12 @@
+package com.ruolan.springtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringtestApplication.class, args);
+	}
+}
---
+++ src/test/java/com/ruolan/springtest/SpringtestApplicationTests.java
@@ -0,0 +1,16 @@
+package com.ruolan.springtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringtestApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
